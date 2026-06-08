2d047ed1e13f9e28676d4850f6092ba449be6267
Nate River <river177169@proton.me>
2026-06-08 21:41:56 +0530

    update

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
+import { log } from "node:console";
+import { Resend } from "resend";
 
 
-async function main(): Promise<Response> {
-  const html_path=new URL("./index.html",import.meta.url);
-  const html=await Deno.readTextFile(html_path);
+const html_path=new URL("./index.html",import.meta.url);
+const html=await Deno.readTextFile(html_path);
 
+Deno.serve(async (req)=> {
+  const path=new URL(req.url).pathname;
+  if(path=="/xd") {
+    const txt=await req.text();
+    handle_email(txt);
+  }
+
+  return main();
+});
+
+function main() {
   return new Response(html,{
     headers: {
       "content-type": "text/html",
@@ -11,6 +23,21 @@ async function main(): Promise<Response> {
   });
 }
 
-Deno.serve(main)
+function handle_email(email: string) {
+  const api_key=Deno.env.get("API_KEY");
+
+  const resend = new Resend(api_key);
+  log(api_key);
+
+  resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'kakashiwebsite69@gmail.com',
+    subject: 'Plan',
+    html: email
+  });
+}
+
+
+
 
 
